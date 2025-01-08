704ee7ce65b6750c1ff4bbeda2f514e9a6e22226
gnanajeyam <gnanajeyam95@gmail.com>
2025-01-08 17:06:14 +0530

    Updated Main

--- src/main/java/org/game/snakeladder/Main.java
+++ src/main/java/org/game/snakeladder/Main.java
@@ -8,30 +8,31 @@ import org.game.snakeladder.models.Position;
 import org.game.snakeladder.service.LadderService;
 import org.game.snakeladder.service.SnakeService;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting the Snake and Ladder Game!");
 
         var snakeService = new SnakeService();
         var ladderService = new LadderService();
-        GameBoard gameBoard = new GameBoard(snakeService, ladderService, new Board[10][10]);
+        var gameBoard = new GameBoard(snakeService, ladderService, new Board[10][10]);
         gameBoard.startGame();
 
-        Player player1 = new Player("Jeyam", new Position(0, 0));
-        Player player2 = new Player("Stunna", new Position(0, 0));
+        var playerQueue = new LinkedList<>(List.of(
+            new Player("Jeyam", new Position(0, 0)),
+            new Player("Stunna", new Position(0, 0))
+        ));
 
-        while (true) {
-            gameBoard.rollDice(player1);
-            if (player1.isWon()) {
-                System.out.printf("Player %s won \n", player1.getName());
+        while (!playerQueue.isEmpty()) {
+            var currentPlayer = playerQueue.poll();
+            gameBoard.rollDice(currentPlayer);
+            if (currentPlayer.isWon()) {
+                System.out.printf("Player %s won \n", currentPlayer.getName());
                 break;
             }
-
-            gameBoard.rollDice(player2);
-            if (player2.isWon()) {
-                System.out.printf("Player %s won \n", player2.getName());
-                break;
-            }
+            playerQueue.add(currentPlayer);
         }
 
         System.out.println("Game Over!");
